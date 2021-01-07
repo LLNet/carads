640322aa235950fa079120522a0d6e762b7e83b9
Morten Bak <meb@indexed.dk>
2021-01-07 13:12:24 +0100

    add elementor module

---
+++ elementor/custom_widgets.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Indexed;
+
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
+
+use Elementor\Plugin;
+
+add_action('elementor/widgets/widgets_registered', function () {
+
+    require_once('widgets/carads-slider.php');
+    Plugin::instance()->widgets_manager->register_widget_type(new CarAdsSlider());
+
+});
