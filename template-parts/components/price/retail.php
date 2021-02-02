67c162ea3e03bbe1c1b1f0ec9e1253852a4307c0
Morten Bak <meb@indexed.dk>
2021-02-02 16:35:32 +0100

    add partials for leasing/retail price

---
+++ template-parts/components/price/retail.php
@@ -0,0 +1,3 @@
+<span class="ca-text-xl md:ca-text-2xl ca-mt-2 lg:ca-mt-0 ca-font-medium">
+<?php echo number_format_i18n($product->pricing->{$this->getCurrency()}->price) . " " . $this->getCurrency(); ?>
+</span>
